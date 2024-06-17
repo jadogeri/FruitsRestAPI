51a64028e4a8479942367a4c71d18a4140e1261d
OrleansParishIT <jadogeri@uno.edu>
2024-06-17 11:03:45 -0500

    Fixed bugs in clientController.js

--- src/controller/clientController.js
+++ src/controller/clientController.js
@@ -313,26 +313,85 @@ const removeFruitsFromClient = asyncHandler(async (req, res) => {
 
 const replaceFruitFromClient = asyncHandler(async (req, res) => {
 
-    // const {email, password,username} = req.body
-    // if(!password && (!username || !email)){
-    //     res.status(404);
-    //     throw new Error('All fields are mandatory')
-    // }
-    // const user = await User.findOne({ email });
-    // if(user && (await bcrypt.compare(password, user.password))){
-    //     const accessToken = jwt.sign({
-    //         user:{
-    //             username : user.username,
-    //             email : user.email,
-    //             id: user.id,
-    //         },
+ 
+    try{
+        const {old_id, new_id} = req.body;
+    
+        if(!old_id || !new_id){
+            res.status(400)
+            throw new Error('both old id and new id fields are mandatory');        
+        }
+        let authHeader = req.headers.authorization || req.headers.Authorization;
+        console.log("autheader === ",authHeader)
+    
+        let token = res.locals.token;
+        console.log("res token from header === ",token)
+    
+        let  {getConnection} = require("../../server");
+        let database = getConnection();
+    
+        //find user in auth associated with the token
+        const authUser = database.get("auth").find({token : token}).value();
+        console.log("auth user === ",JSON.stringify(authUser))
+        if(!authUser){
+            res.status(400) //.json({message:"all fields must be filled"})
+            throw new Error(`no user accociated with token`);
+        }
+        //use old id to find fruit from fruits table
+        const fruits = database.get("fruits").value();
+        console.log("fruit in fruits table === ",JSON.stringify(fruits));
+        const old_fruit = fruits.filter((item)=>{
+            return item.id == old_id
+        })
+        if(!old_fruit){
+            res.status(400) //.json({message:"all fields must be filled"})
+            throw new Error(`no fruit in database accociated with id ${old_id}`);
+        }
+        //use new id to find fruit from fruits table
+        const new_fruit = fruits.filter((item)=>{
+            return item.id == new_id
+        })
+        if(!new_fruit){
+            res.status(400) //.json({message:"all fields must be filled"})
+            throw new Error(`no fruit in database accociated with id ${new_id}`);
+        }
+        //use the username in auth table to find user in the user table
 
-    //     } , process.env.ACCESS_TOKEN_SECRET,{expiresIn : "15m"} );
-    //     res.status(200).json({accessToken})
-    // }else{
-    //     res.status(401);
-    //     throw new Error('password or email not valid');
-    // }
+        const {username : authUsername} = authUser
+        console.log("username in auth === ",authUsername)
+        const user = database.get("users").find({username : authUsername}).value();
+        console.log("user in users table === ",JSON.stringify(user))
+        const {fruits : userFruits} = user;
+        console.log("user fruits === ",JSON.stringify(userFruits))
+    
+        //check if user has old fruit in the list
+        const oldFruitFound = userFruits.filter((item)=>{
+            if(old_id == item.id){
+                return item;
+            }
+        })
+        console.log("fruit found length === ",oldFruitFound.length)
+        console.log("old fruit found ===",JSON.stringify(oldFruitFound))
+    
+        if(oldFruitFound.length === 0){
+            res.status(400) //.json({message:"all fields must be filled"})
+            throw new Error(`fruit with id ${old_id} not in ${user.username}'s list`);
+        }    
+        //use map function to return new array and only update old fruit id with new fruit id
+        
+        const newFruitsList =  userFruits.map((item) => {
+            
+            if(item.id == old_id){
+                return new_fruit  
+            }
+            return item
+           });
+           console.log(JSON.stringify(newFruitsList))
+        const result = database.get('users').find({username : user.username })
+                               .assign({fruits : newFruitsList})
+                               .write();
+        res.json(newFruitsList)
+    }catch(e){console.log(e)}
     res.json({ message: 'replace fruit from the client'})
 
 });
