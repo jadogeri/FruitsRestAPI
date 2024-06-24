18645247b7717115b72d25862ba98493544bf94f
OrleansParishIT <jadogeri@uno.edu>
2024-06-24 16:16:09 -0500

    updated clientDoc.js

--- server/src/docs/clientDoc.js
+++ server/src/docs/clientDoc.js
@@ -1,3 +1,25 @@
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoieiIsImlkIjoiNCJ9LCJpYXQiOjE3MTkyNDg5ODcsImV4cCI6MTcxOTI1MTk4N30.FKogDmhGl2eh-IsSuvOcr2K3kzC4JdJpAyjvqe4dV54
+
+
+
+/**
+ * 
+ * 
+ * router.get("/getfruits/",validateTokenHeader,getFruitsFromClient);
+
+router.get("/getfruit/:id",validateTokenHeader,getFruitFromClient);
+
+router.post("/addfruit/",validateTokenHeader,addFruitToClient);
+
+router.put("/replacefruit/",validateTokenHeader,replaceFruitFromClient);
+
+router.delete("/removefruit/:id",validateTokenHeader,removeFruitFromClient);
+
+router.delete("/removefruits/",validateTokenHeader,removeFruitsFromClient);
+
+
+ */
+
 /**
  * @swagger
  * components:
@@ -25,6 +47,8 @@
  *           type: string
  *           format: date
  *           description: The date the client was added
+ * security:
+ *   fruitsrestAuth: []
  *     
  */
 /**
@@ -32,9 +56,9 @@
  * tags:
  *   name: Clients
  *   description: The Clients managing API
- * /client:
+ * /api/client/getfruits:
  *   get:
- *     summary: Lists all the Clients
+ *     summary: Lists all Fruits of the Client
  *     tags: [Clients]
  *     responses:
  *       200:
@@ -45,16 +69,27 @@
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/Clients'
- * /api/client/addClient:
+ *       500:
+ *         description: Some server error
+ *     security:
+ *       - fruitsrestAuth:
+ *           - write:clients
+ *           - read:clients  
+ * /api/client/addFruit:
  *   post:
- *     summary: Create a new client
+ *     summary: Add a fruit to a new client
  *     tags: [Clients]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/Clients'
+ *             type: object
+ *             required: [id] 
+ *             properties:
+ *               id:
+ *                  type: integer
+ *                  example: 1
  *     responses:
  *       200:
  *         description: The created client.
@@ -64,9 +99,13 @@
  *               $ref: '#/components/schemas/Clients'
  *       500:
  *         description: Some server error
- * /client/{id}:
+ *     security:
+ *       - fruitsrestAuth:
+ *           - write:clients
+ *           - read:clients  
+ * /api/client/getFruit/{id}:
  *   get:
- *     summary: Get the client by id
+ *     summary: Get the client specific fruit by id
  *     tags: [Clients]
  *     parameters:
  *       - in: path
@@ -84,6 +123,12 @@
  *               $ref: '#/components/schemas/Clients'
  *       404:
  *         description: The client was not found
+ *       500:
+ *         description: Some server error
+ *     security:
+ *       - fruitsrestAuth:
+ *           - write:clients
+ *           - read:clientss  
  *   put:
  *    summary: Update the client by the id
  *    tags: [Clients]
